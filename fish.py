d3bf1e2eefedee0aff29e31d230e2ba905abe551
Jadbinder Singh Toor <jadbindertoor@gmail.com>
2024-04-09 12:25:52 +0200

    1. Commit

---
+++ fish.py
@@ -0,0 +1,19 @@
+import vector as v
+import pygame
+
+class Fish:
+    def __init__(self, position, velocity, screen):
+        self.position = position
+        self.velocity = velocity
+        self.screen = screen
+        self.fishy = pygame.image.load('fish_image.png')
+        self.fishy = pygame.transform.scale(self.fishy, (40,40))
+        
+    def update(self, position, velocity):
+        self.position = self.position + self.velocity
+
+    def draw(self):
+        self.screen.blit(self.fishy,(self.position.x,self.position.y))
+        
+
+     
