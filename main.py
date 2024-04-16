7d12a9ca759b16c03365886e35f4588ec1d5bfb1
Jadbinder Singh Toor <jadbindertoor@gmail.com>
2024-04-16 11:01:00 +0200

    7. commit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
+import sys
 from vector import Vector
 from fish import Fish
+from slider import Slider
 
 # Start game
 pygame.init()
@@ -9,8 +11,12 @@ pygame.init()
 WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-# Create 5 fish objects
-fish_list = [Fish(screen) for _ in range(5)]
+# Create slider
+slider = Slider(50, 50, 300, 20, 1, 10, 5, "Number of Fish")
+
+# Initial number of fish
+num_fish = int(slider.get_value())
+fish_list = [Fish(screen) for _ in range(num_fish)]
 
 # Game loop
 running = True
@@ -22,10 +28,20 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
+        slider.handle_event(event)  # Handle slider events
+
+    # Update number of fish based on slider value
+    new_num_fish = int(slider.get_value())
+    if new_num_fish != num_fish:
+        fish_list = [Fish(screen) for _ in range(new_num_fish)]
+        num_fish = new_num_fish
+
     for fish in fish_list:
         fish.update()
         fish.draw()
+
+    # Draw slider
+    slider.draw(screen)
 
     pygame.display.flip()
 
