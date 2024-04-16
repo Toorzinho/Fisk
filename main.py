a1ca7ae59cf3ab591789b7ca13b10f08570761c8
Jadbinder Singh Toor <jadbindertoor@gmail.com>
2024-04-16 10:49:58 +0200

    6. commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from vector import Vector
-from fish import Fish, generate_random_fish
+from fish import Fish
 
 # Start game
 pygame.init()
@@ -9,7 +9,8 @@ pygame.init()
 WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-fish_list = generate_random_fish(screen)
+# Create 5 fish objects
+fish_list = [Fish(screen) for _ in range(5)]
 
 # Game loop
 running = True
